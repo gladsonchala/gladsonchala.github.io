aeae92bbc6ae913990d30be6a70c944d98fa2603
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2024-02-02 02:08:25 +0300

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.getElementById("nav-toggle");
-const navMenu = document.querySelector("nav ul");
-
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
